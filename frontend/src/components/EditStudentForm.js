6e73f852d439931bf42868a2e21a06fb19fe2ab4
siri palakurthi <palakurthysiri@gmail.com>
2025-04-29 17:29:26 +0530

    Update EditStudentForm.js

--- frontend/src/components/EditStudentForm.js
+++ frontend/src/components/EditStudentForm.js
@@ -23,7 +23,7 @@ import React, { useState, useEffect } from 'react';
 
           const fetchStudent = async () => {
             try {
-              const response = await axios.get(`https://studentsystem-backend.onrender.com/students/${id}`);
+              const response = await axios.get(`https://studentsystem-frontend.onrender.com/students/${id}`);
               setStudent(response.data);
             } catch (error) {
               console.error('Error fetching student:', error);
@@ -38,7 +38,7 @@ import React, { useState, useEffect } from 'react';
           const handleSubmit = async (e) => {
             e.preventDefault();
             try {
-              await axios.put(`https://studentsystem-backend.onrender.com/students/${id}`, student);
+              await axios.put(`https://studentsystem-frontend.onrender.com/students/${id}`, student);
               Swal.fire('Success!', 'Student updated successfully!', 'success');
               navigate('/students');
             } catch (error) {
