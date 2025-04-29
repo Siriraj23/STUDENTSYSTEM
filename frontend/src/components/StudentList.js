83739f9986f367b5b224e4cbcece173a0e42fcd7
siri palakurthi <palakurthysiri@gmail.com>
2025-04-29 17:30:03 +0530

    Update StudentList.js

--- frontend/src/components/StudentList.js
+++ frontend/src/components/StudentList.js
@@ -12,7 +12,7 @@ const StudentList = () => {
 
   const fetchStudents = async () => {
     try {
-      const response = await axios.get('https://studentsystem-backend.onrender.com/students');
+      const response = await axios.get('https://https://studentsystem-frontend.onrender.com/students');
       setStudents(response.data);
     } catch (error) {
       console.error('Error fetching students:', error);
@@ -32,7 +32,7 @@ const StudentList = () => {
     }).then(async (result) => {
       if (result.isConfirmed) {
         try {
-          await axios.delete(`https://studentsystem-backend.onrender.com/students/${id}`);
+          await axios.delete(`https://studentsystem-frontend.onrender.com/students/${id}`);
           fetchStudents();
           Swal.fire('Deleted!', 'Student has been deleted.', 'success');
         } catch (error) {
