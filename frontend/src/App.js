461ac1a6601b05b5bbd4d95e9ed5b44542e0c4cb
siri palakurthi <palakurthysiri@gmail.com>
2025-04-29 17:43:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,39 @@
-// frontend/src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import StudentList from './components/StudentList';
 import AddStudentForm from './components/AddStudentForm';
 import EditStudentForm from './components/EditStudentForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'; // Import your custom CSS file
 
 function App() {
   return (
     <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/students">
+                  View Students
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/add">
+                  Add Student
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
